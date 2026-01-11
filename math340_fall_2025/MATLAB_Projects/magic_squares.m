1a178b2f0362919fad1d4a558f4dc0c0bb0ecb9a
Troy Chin <148022362+troyc03@users.noreply.github.com>
2026-01-11 14:12:23 -0800

    Delete math340_fall_2025 directory

--- math340_fall_2025/MATLAB_Projects/magic_squares.m
+++
@@ -1,8 +0,0 @@
-function magic
-
-% Initialize a 3x3 magic square
-A = magic(3);
-for k = 0:3
-    rot90(A,k)
-    rot90(A', k)
-end
--- math340_fall_2025/MATLAB_Projects/powersin.m
+++
@@ -1,9 +0,0 @@
-function s = powersin(x)
-s = 0;
-t = x;
-n = 1;
-while s + t ~= s
-    s = s + t;
-    t = x.^2/((n+1)*(n+2)).*t
-    n = n + 2;
-end
